8d50d55ba68bf4c946621da23bde9b5ddbfb42f0
Nick Zárate <nickzaro@gmail.com>
2021-06-17 11:24:04 -0300

    Commit inicial, configuración del proyecto

---
+++ src/test/java/com/nick/junitmockitospringboot/JunitMockitoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nick.junitmockitospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitMockitoSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
